65b0cddb76e158a08ace07e0c4254846dcedd263
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2022-12-30 20:10:07 +0300

    commit test-count 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import s from './App.module.scss';
-import {Btn} from "./Btn/Btn";
-import {Input} from './Input/Input';
+import {Settings} from "./Settings/Settings";
+import {Count} from "./Count/Count";
 
 function App() {
 
 
-    const onClickInc = () => {
-        minValue < maxValue &&  maxValue > a ? setA(a + 1) : setA(a)
-    }
-    const onClickReset = () => {
-        setA(minValue)
-    }
-    const onCLickAlert = () => alert('INCORRECT MIN_VALUES')
+
     //======================================================================================================
     let [maxValue, setMaxValue] = useState<number>(5)
     let [minValue, setMinValue] = useState<number>(0)
-    let [a, setA] = useState(minValue)
-
-    useEffect( ()=> {
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-        localStorage.setItem('minValue', JSON.stringify(minValue))
-    }, [maxValue,minValue])
-
+    let [currentCountValue, setCurrentCountValue] = useState(minValue)
 
     useEffect(()=> {
         let valueStrMax = localStorage.getItem('maxValue')
         let valueStrMin = localStorage.getItem('minValue');
-
         console.log(valueStrMax ? JSON.parse(valueStrMax) : 'lol')
         console.log(valueStrMin ? JSON.parse(valueStrMin) : 'lol')
 
         valueStrMax !== null ? setMaxValue(JSON.parse(valueStrMax)) : setMaxValue(6);
         valueStrMin !== null ? setMinValue(JSON.parse(valueStrMin)) : setMinValue(1);
+        setCurrentCountValue(valueStrMin ? JSON.parse(valueStrMin) : minValue)
+
     }, [])
-
-
-
-    const onCLickButtonSet = ( ) => {
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-        localStorage.setItem('minValue', JSON.stringify(minValue))
-    }
-
-    const onChangeMaxHandler = (numMax: number) => {
-        numMax > 0 ? setMaxValue(numMax) : setMaxValue(1)
-    }
-    const onChangeMinHandler = (numMin: number) => {
-        numMin > 0  ? setMinValue(numMin) : setMinValue(0)
-        setA(numMin < 0 ? 0: numMin)
-    }
-
-
-
+    // useEffect( ()=> {
+    //     localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    //     localStorage.setItem('minValue', JSON.stringify(minValue))
+    // }, [maxValue,minValue])
 
     return (
-
         <div className={s.container}>
+            <Settings
+                setMaxValue={setMaxValue}
+                setMinValue={setMinValue}
+                maxValue={maxValue}
+                minValue={minValue}
+                setCurrentCountValue={setCurrentCountValue}
+            />
 
-            <div className='settings'>
-                <div className="settings__title">
-                    <div className={'settings__item'}>
-                        <div className="settings__name">max-value</div>
-                        <Input onChangeHandler={onChangeMaxHandler} numberValue={maxValue}/>
-                    </div>
-                    <div className={'settings__item'}>
-                        <div className="settings__name">min-value</div>
-                        <Input onChangeHandler={onChangeMinHandler} numberValue={minValue} />
-                        {maxValue <= minValue && <div className={s.errorDiv}>INCORRECT VALUE</div>}
-                    </div>
-                </div>
-                <div>
-                    {maxValue <= minValue
-                        ? <button disabled={true}>SetValues</button>
-                        : <Btn className={s.button} name={'SetValues'} onClick={onCLickButtonSet}/>
-                    }
-
-                </div>
-            </div>
-
-
-
-
-            <div className={s.count}>
-                <div className={s.count__title}>
-                    {minValue >= maxValue
-                        ? <h1>INCORRECT COUNT</h1>
-                        : <h1>Count</h1>
-                    }
-                    <h1 className={`${a >= maxValue ? s.redBred : ''}`}>{a}</h1>
-                </div>
-                <div className={s.count__btn}>
-                    <Btn className={`${s.inc} + ${a >= maxValue ? s.disabled : ''}`} onClick={onClickInc} name={"Inc"}/>
-                    {maxValue <= minValue
-                        ? <Btn className={`${s.reset} + ${maxValue <= minValue ? s.disabled : ''}`} onClick={onCLickAlert} name={"Reset"}/>
-                        : <Btn className={`${s.reset} + ${a === minValue ? s.disabled : ''}`} onClick={onClickReset} name={"Reset"}/>
-                    }
-                </div>
-            </div>
+            <Count maxValue={maxValue}
+                   minValue={minValue}
+                   setCurrentCountValue={setCurrentCountValue}
+                   currentCountValue={currentCountValue}
+            />
         </div>
-
     );
-
-
 }
 
 export default App;
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,8 +1,10 @@
 import s from "./Input.module.scss"
 import {ChangeEvent} from "react";
+import {Grid, TextField} from "@mui/material";
 type PropsType = {
     numberValue: number
     onChangeHandler: (num: number)=> void
+    title: string
 }
 
 export const Input = (props: PropsType) => {
@@ -12,7 +14,19 @@ export const Input = (props: PropsType) => {
     }
     return(
         <div className={s.wrapper}>
-            <input type="number" value={props.numberValue} onChange={onChangeNum}/>
+            <Grid container spacing={0}>
+                <TextField
+                    className={s.input}
+                    label={props.title}
+                    type='number'
+                    size={"small"}
+                    value={props.numberValue}
+                    onChange={onChangeNum}
+                    variant={'outlined'}
+                    color={"secondary"}
+                />
+            </Grid>
+            {/*<input type="number" value={props.numberValue} onChange={onChangeNum}/>*/}
         </div>
     )
 }
